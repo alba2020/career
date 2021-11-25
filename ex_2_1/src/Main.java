75ab33f41712c46329c2d09c42de0f2d93572517
alba2020 <alba2020@gmail.com>
2021-11-25 22:00:46 +0300

    wip

--- ex_2_1/src/Main.java
+++ ex_2_1/src/Main.java
@@ -1,5 +1,39 @@
+import java.util.HashSet;
+
 public class Main {
+    public static void print(Node start) {
+        Node n = start;
+        while(n != null) {
+            System.out.printf("%d ", n.data);
+            n = n.next;
+        }
+        System.out.println();
+    }
+
+    public static Node removeDups(Node head) {
+        Node n = head;
+        HashSet<Integer> h = new HashSet<>();
+        h.add(n.data);
+        while (n.next != null) {
+            if (h.contains(n.next.data)) {
+                n.next = n.next.next;
+            } else {
+                h.add(n.next.data);
+            }
+            n = n.next;
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
-        System.out.println(1);
+        Node head1 = new Node(1);
+        head1.appendToTail(2);
+        head1.appendToTail(3);
+        head1.appendToTail(2);
+        head1.appendToTail(2);
+        print(head1);
+
+        removeDups(head1);
+        print(head1);
     }
 }
